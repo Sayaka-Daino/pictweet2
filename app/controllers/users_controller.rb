77953830f95db9fbc6514ba0f2b33413d2a9992e
Sayaka-Daino <gcp.syk415@gmail.com>
2015-10-04 18:51:13 +0900

    アソシエーションを利用する

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
   def show
-
     @nickname = current_user.nickname
     @tweets = Tweet.where(user_id: current_user.id).page(params[:page]).per(5).order("created_at DESC")
+
+    @nickname = current_user.nickname
+    @tweets = current_user.tweets.page(params[:page]).per(5).order("created_at DESC")
   end
 end
